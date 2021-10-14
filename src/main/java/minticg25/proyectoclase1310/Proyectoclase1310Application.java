326a8219445d9fa5c9b6bf819def71fb84ad0968
ANGIE ALDANA PADILLA <aaldanap@ucentral.edu.co>
2021-10-13 19:35:28 -0500

    proyecto springboot

---
+++ src/main/java/minticg25/proyectoclase1310/Proyectoclase1310Application.java
@@ -0,0 +1,13 @@
+package minticg25.proyectoclase1310;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectoclase1310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyectoclase1310Application.class, args);
+	}
+
+}
---
+++ src/test/java/minticg25/proyectoclase1310/Proyectoclase1310ApplicationTests.java
@@ -0,0 +1,13 @@
+package minticg25.proyectoclase1310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectoclase1310ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
